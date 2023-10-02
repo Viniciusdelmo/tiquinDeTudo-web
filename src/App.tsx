d79701a1014f871eef2f67840726635b2a17a624
Vinícius Delmo <103666432+Viniciusdelmo@users.noreply.github.com>
2023-10-02 17:51:54 -0300

    feat: adding header

---
+++ src/App.tsx
@@ -0,0 +1,24 @@
+
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import "./App.css";
+
+//import Home from "./pages/home";
+
+import Header from "./components/Header/Header";
+
+
+
+function App() {
+  return (
+    <BrowserRouter>
+      <Header />
+      <div className="App" style={{ flex: 1 }}>
+        <Routes>
+          {/* <Route path="/" element={<Home />} /> */}
+        </Routes>
+      </div>
+    </BrowserRouter>
+  );
+}
+
+export default App;
---
+++ src/components/Header/Header.tsx
@@ -0,0 +1,60 @@
+import "./styles.scss";
+import { Link } from "react-router-dom";
+
+const Header = () => {
+  return (
+    <div className="header-container">
+      <a href="/">
+        <div className="logo"><img src="/Logo-tiquin.svg" alt="" /></div>
+      </a>
+      <div className="firstMenu">
+        <nav>
+          <ul>
+            <li>
+              <Link to="/" className="link-styled">
+                Home
+              </Link>
+            </li>
+            <li>
+              <Link to="/products" className="link-styled">
+                Products
+              </Link>
+            </li>
+            <li>
+              <Link to="/contacts" className="link-styled">
+                Contacts
+              </Link>
+            </li>
+          </ul>
+        </nav>
+      </div>
+      <div className="secondMenu">
+        <nav>
+          <ul>
+            <a href="/empty">
+              <button>
+                <img src="/Header/shoppingCart.svg" alt="Carrinho de compras" />
+              </button>
+            </a>
+            <a href="/empty">
+              <button>
+                {" "}
+                <img src="/Header/user.svg" alt="user" />
+              </button>
+            </a>
+            <li>
+              <img src="/Header/divider.svg" alt="" />
+            </li>
+            <a href="/empty">
+              <button>
+                <img src="/Header/filter.svg" alt="filter" />
+              </button>
+            </a>
+          </ul>
+        </nav>
+      </div>
+    </div>
+  );
+};
+
+export default Header;
